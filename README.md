bbb1297d80084751c8807633737b87973b8f2491
William DURAND <william.durand1@gmail.com>
2012-02-28 22:03:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Speaker
+=======
+
+Convert a text file in an `mp3` audio file.
+
+
+Dependencies
+------------
+
+You need the `say` command which is bundled with Mac OSX, and `ffmpeg` you can install by running:
+
+    brew install --use-clang --HEAD ffmpe
+
+
+Usage
+-----
+
+    USAGE:
+      ./speaker [-h] [-d <output directory>] <filename>
+
+
+Credits
+-------
+
+* William Durand <william.durand1@gmail.com>
+
+
+License
+-------
+
+Speaker is released under the MIT License. See the bundled `LICENSE` file for details.
---
+++ speaker-test.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env roundup
+#
+# speaker-test.sh | William Durand <william.durand1@gmail.com>
+
+describe "speaker"
+
+speaker="./speaker"
+input_file="dummy-post-title.markdown"
+mp3_file="dummy-post-title.mp3"
+output_dir="output_directory"
+new_output_dir="new_output_directory"
+
+before() {
+    echo "dummy content in my awesome post" > $input_file
+    mkdir $output_dir
+}
+
+after() {
+    rm -rf $input_file $mp3_file
+    rm -rf $output_dir $new_output_dir
+}
+
+it_shows_help_with_no_argv() {
+    $speaker | grep USAGE
+}
+
+it_shows_help_with_h_option() {
+    $speaker -h | grep USAGE
+}
+
+it_creates_mp3_file() {
+    $speaker $input_file
+
+    test -f $mp3_file
+}
+
+it_creates_mp3_file_in_existing_directory() {
+    $speaker -d $output_dir $input_file
+
+    test -f "$output_dir/$mp3_file" && test ! -f $mp3_file
+}
+
+it_creates_mp3_file_in_new_directory() {
+    $speaker -d $new_output_dir $input_file
+
+    test -f "$new_output_dir/$mp3_file" && test ! -f $mp3_file
+}
